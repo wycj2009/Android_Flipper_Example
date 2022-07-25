68b07194e1409625dab1f2ef78bc09485615d8b3
wycj2009 <wycj2009@naver.com>
2022-07-25 17:57:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/android_flipper_example/App.kt
@@ -0,0 +1,22 @@
+package com.example.android_flipper_example
+
+import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.soloader.SoLoader
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        SoLoader.init(this, false)
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            AndroidFlipperClient.getInstance(this).apply {
+                addPlugin(InspectorFlipperPlugin(this@App, DescriptorMapping.withDefaults()))
+            }.start()
+        }
+    }
+}
